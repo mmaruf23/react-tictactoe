4b3f952e81ae1dd91c54fef7e1eb9f57627ebf2a
mmaruf23 <mmaruf23@termux>
2025-03-07 16:22:14 +0700

    disabling multi

--- src/pages/MultiMode.jsx
+++ src/pages/MultiMode.jsx
@@ -1,20 +1,29 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Tile from "../components/Tile";
 import { useOnline } from "../hooks/useOnline";
 import Navbar from "../components/Navbar";
 import Board from "../components/Board";
-
+import { useNavigate } from "react-router";
 
 export default function MultiMode() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const user = localStorage.getItem("user") || "Guest";
   const isOnline = useOnline();
+  const navigate = useNavigate();
 
   const handleClick = (i) => {
     console.log(i);
   };
-
+  
+  useEffect(() => {
+    alert("MultiMode is still development")  
+    navigate({
+      pathname: "/"
+    });
+    
+  }, []);
+  
 
   return (
     <div className="h-screen w-screen bg-black text-white flex justify-center">
@@ -26,4 +35,4 @@ export default function MultiMode() {
     </div>
   );
 }
-
+ 
